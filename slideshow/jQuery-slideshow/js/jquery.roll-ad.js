7a631fad428ca373f1c66c6796930e2357a3790f
MrMengJ <2646973632@qq.com>
2017-06-22 23:14:04 +0800

    完成左右按钮轮播
    完成左右按钮轮播

--- slideshow/jQuery-slideshow/js/jquery.roll-ad.js
+++ slideshow/jQuery-slideshow/js/jquery.roll-ad.js
@@ -14,6 +14,9 @@
         this.rollBtn = rollAd.find('.roll-btn');
 
         this.rollItemLen = this.rollItems.length;
+
+        this.switchover = rollAd.find('.switchover');
+        this.switchoverItem = rollAd.find('.switchover-item');
         //默认参数
         this.setting = {
             "width": 800,
@@ -41,6 +44,16 @@
             }
         });
 
+        this.switchoverItem.hover(
+            function () {
+                $(this).addClass('active').siblings().removeClass('active');
+                self.switchoverAnimate($(this).index());
+            }, function () {
+                 console.log("离开了");
+            }
+        )
+
+
         // this.autoRoll();
         /*鼠标进入轮播图范围，停止自动轮播，两侧按钮淡入*/
         /*鼠标离开轮播图范围，开始自动轮播，两侧按钮淡出*/
@@ -51,6 +64,7 @@
             // self.autoRoll();
             self.rollBtn.fadeOut(600);
         });
+
 
     };
     /*构造函数Rollad的prototype属性*/
@@ -86,20 +100,21 @@
 
             this.secondRollItem.css({
                 "left": rollWidth - liWidth,
-                "opacity": 0.5,
+                // "opacity": 0.5,
                 "zIndex": 22,
                 "top": 10
             });
 
             this.lastRollItem.css({
                 "left": 0,
-                "opacity": 0.5,
+                // "opacity": 0.5,
                 "zIndex": 22,
                 "top": 10
             });
         },
 
         "nowflag": 0,
+        "oldflag": null,
 
         rotateAnimate: function (type) {
 
@@ -123,54 +138,64 @@
             // console.log("nowflag:" + this.nowflag);
             var that = this;
             var nextEle = nowEle.next().get(0) ? nowEle.next() : this.firstRollItem;
-            var prev = nowEle.prev().get(0) ? nowEle.prev() : this.lastRollItem;
+            var prevEle = nowEle.prev().get(0) ? nowEle.prev() : this.lastRollItem;
 
             if (type == 'next') {
                 // this.getEleCss(nextEle);
                 var nextEleTwo = nextEle.next().get(0) ? nextEle.next() : this.firstRollItem;
-
+                // nextEleTwo.animate(this.getEleZIndex(nextEle), 0);
                 nextEleTwo.animate(this.getEleCss(nextEle), delay, function () {
                     that.rollFlag = true;
                     // console.log(that.getEleCss(nextEle));
                 });
                 // console.log(this.getEleCss(nextEle))
-
+                // nextEle.css('display','none');
+                nextEle.animate(this.getEleZIndex(nowEle), 0);
+                // nextEle.fadeIn(delay);
                 nextEle.animate(this.getEleCss(nowEle), delay, function () {
                     that.rollFlag = true;
                 }).attr('nowflag', ++this.nowflag);
-
+                setTimeout(function () {
+                    nextEle.addClass('active').siblings().removeClass('active');
+                    console.log("hello");
+                }, 100);
                 // nowEle.css("opacity","22");
-                nowEle.animate(this.getEleCss(prev), delay, function () {
+                // nextEleTwo.animate(this.getEleZIndex(prev), 0);
+                nowEle.animate(this.getEleCss(prevEle), delay, function () {
                     that.rollFlag = true;
                 });
 
-                prev.animate({
+                prevEle.animate({
                     "width": liWidth,
                     "height": liHeight,
                     "zIndex": 0,
                     "left": "140px",
-                    "opacity": 0,
-                    // "top": "10px"
+                    // "opacity": 0,
+                    "top": "10px"
                 }, delay, function () {
                     that.rollFlag = true;
                 });
-                console.log("nowflag"+this.nowflag)
+                console.log("nowflag" + this.nowflag);
 
             }
 
             if (type == 'prev') {
-                var prevEleTwo = prev.prev().get(0) ? prev.prev() : this.lastRollItem;
+                var prevEleTwo = prevELe.prev().get(0) ? prev.prev() : this.lastRollItem;
 
-
+                // prevEleTwo.animate(this.getEleZIndex(prev), 0);
                 prevEleTwo.animate(this.getEleCss(prev), delay, function () {
                     that.rollFlag = true;
                 });
-
+                prev.animate(this.getEleZIndex(nowEle), 0);
                 prev.animate(this.getEleCss(nowEle), delay, function () {
                     that.rollFlag = true;
                 }).attr('nowflag', --this.nowflag);
+                setTimeout(function () {
+                    prevEle.addClass('active').siblings().removeClass('active');
+                }, 200);
 
                 // nowEle.css("opacity","22");
+                // nowEle.animate(this.getEleZIndex(nextEle),0);
                 nowEle.animate(this.getEleCss(nextEle), delay, function () {
                     that.rollFlag = true;
                 });
@@ -180,13 +205,85 @@
                     "height": liHeight,
                     "zIndex": 0,
                     "left": "140px",
-                    "opacity": 0,
+                    // "opacity": 0,
                     "top": "10px"
                 }, delay, function () {
                     that.rollFlag = true;
                 });
 
             }
+        },
+        switchoverAnimate: function (nowIndex) {
+            var that = this;
+            var rollWidth = this.setting.width;
+            var rollHeight = this.setting.height;
+            var liWidth = this.setting.liWidth;
+            var liHeight = this.setting.liHeight;
+            var delay = this.setting.delay;
+            this.oldflag = this.nowflag;
+            this.nowflag = nowIndex;
+            console.log(this.oldflag);
+            console.log(this.nowflag);
+            if (parseInt(this.nowflag) >= this.rollItemLen) {
+
+                this.nowflag = 0;
+
+            } else if (parseInt(this.nowflag) < 0) {
+
+                this.nowflag = this.rollItemLen - 1;
+            }
+            var oldEle = this.rollItems.eq(this.oldflag);
+            var nowEle = this.rollItems.eq(this.nowflag).attr('nowflag',this.nowflag).siblings().removeAttr('nowflag');
+            // console.log("nowEle:" + nowEle);
+            // console.log("nowflag:" + this.nowflag);
+            var oldNextEle = oldEle.next().get(0) ? nowEle.next() : this.firstRollItem;
+            var nextEle = nowEle.next().get(0) ? nowEle.next() : this.firstRollItem;
+            var oldPrevEle = oldEle.next().get(0) ? nowEle.prev() : this.lastRollItem;
+            var prevEle = nowEle.prev().get(0) ? nowEle.prev() : this.lastRollItem;
+            prevEle.animate(this.getEleCss(oldPrevEle), delay, function () {
+                that.rollFlag = true;
+            });
+            nowEle.animate(this.getEleCss(oldEle), delay, function () {
+                that.rollFlag = true;
+            });
+            setTimeout(function () {
+                    nowEle.addClass('active').siblings().removeClass('active');
+                    console.log("hello");
+                }, 100);
+            nextEle.animate(this.getEleCss(oldNextEle), delay, function () {
+                that.rollFlag = true;
+            });
+
+            oldEle.animate({
+                "width": liWidth,
+                "height": liHeight,
+                "zIndex": 0,
+                "left": "140px",
+                // "opacity": 0,
+                "top": "10px"
+            }, delay, function () {
+                that.rollFlag = true;
+            });
+            oldPrevEle.animate({
+                "width": liWidth,
+                "height": liHeight,
+                "zIndex": 0,
+                "left": "140px",
+                // "opacity": 0,
+                "top": "10px"
+            }, delay, function () {
+                that.rollFlag = true;
+            });
+            oldNextEle.animate({
+                "width": liWidth,
+                "height": liHeight,
+                "zIndex": 0,
+                "left": "140px",
+                // "opacity": 0,
+                "top": "10px"
+            }, delay, function () {
+                that.rollFlag = true;
+            });
         },
         getEleCss: function (ele) {
             var width = ele.css("width"),
@@ -195,7 +292,7 @@
                 opacity = ele.css("opacity"),
                 left = ele.css("left"),
                 top = ele.css("top");
-                // console.log("width:"+width);
+            // console.log("width:"+width);
             return {
                 "width": width,
                 "height": height,
@@ -203,6 +300,12 @@
                 "left": left,
                 "opacity": opacity,
                 "top": top
+            }
+        },
+        getEleZIndex: function (ele) {
+            var zIndex = ele.css('zIndex');
+            return {
+                "zIndex": zIndex + 1
             }
         },
         getSetting: function () {
